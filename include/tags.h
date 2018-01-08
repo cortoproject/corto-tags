38bedb445030ae59890912016951a24d7a8fac06
Sander Mertens <sander.mertens8@gmail.com>
2018-01-07 16:01:44 -0800

    Initial commit

---
+++ include/tags.h
@@ -0,0 +1,29 @@
+/* tags.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
+
+#ifndef TAGS_H
+#define TAGS_H
+
+#include <corto/corto.h>
+#include <corto/corto.h>
+#include <tags/_project.h>
+#include <corto/c/c.h>
+
+/* $header() */
+/* Definitions that are required by package headers (native types) go here. */
+/* $end */
+
+#include <tags/_type.h>
+#include <tags/_interface.h>
+#include <tags/_load.h>
+#include <tags/c/_api.h>
+
+/* $body() */
+/* Definitions here that need your package headers go here. */
+/* $end */
+
+#endif
+
---
+++ src/tags.c
@@ -0,0 +1,8 @@
+/* This is a managed file. Do not delete this comment. */
+
+#include <tags/tags.h>
+
+int cortomain(int argc, char *argv[]) {
+    
+    return 0;
+}
